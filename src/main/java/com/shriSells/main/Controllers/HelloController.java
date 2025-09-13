ea2724a960026ed64194ce3bbb6e28035ae0ec84
Shriganesh Bhide <shriganesh2019@iiitkottayam.ac.in>
2025-09-13 17:26:36 +0530

    Good commit with hidden keys amended commit

--- src/main/java/com/shriSells/main/Controllers/HelloController.java
+++ src/main/java/com/shriSells/main/Controllers/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Mohan ke man ko tu bahye gayi re§§";
+        return "hello World";
     }
 
 }
